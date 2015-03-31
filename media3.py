2a7af64cd86e07d59dcef1b93ed6b5fd1fdfb612
dm2137 <dm2137@users.noreply.github.com>
2015-03-31 16:25:34 -0500

    Committed by Udacity

---
+++ media3.py
@@ -0,0 +1,17 @@
+# define a class called "Movie" to store movie related information
+# identify all the movie information elements to include, that is the title of the movie,
+# the movie poster image, the movie trailer, who stars in the movie, and the movie rating
+
+class Movie():
+     def __init__(self, movie_title, poster_image, trailer_youtube, movie_stars, movie_rating):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.movie_stars = movie_stars
+        self.movie_rating = movie_rating
+
+
+        
+        
+
+    
